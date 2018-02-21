30ad9f50698ac758bb055587887cc1c2e5316d0d
Camila Daniels <camila.daniels926@gmail.com>
2018-02-21 10:16:16 -0600

    Updated server.js file.
    Get and put requests to api/bears are working. Additionally, the get request to api/bears/:id works as well.

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors'); // https://www.npmjs.com/package/cors
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Bear = require('./bears/BearModel.js');
 
 const server = express();
 
@@ -13,7 +15,66 @@ server.get('/', function(req, res) {
   res.status(200).json({ status: 'API Running' });
 });
 
-const port = process.env.PORT || 5005;
-server.listen(port, () => {
-  console.log(`API running on http://localhost:${port}.`);
+server.post('/api/bears', (req, res) => {
+	const bearInformation = req.body;
+
+	if (!bearInformation.species || !bearInformation.latinName) {
+		res.status(400).json({
+			error: 'Please provide both species and a latinName for the Bear.'
+		})
+	} else {
+		const bear = new Bear(bearInformation);
+		bear.save()
+			.then((newBear) => {
+				res.status(201).json(newBear);
+			})
+			.catch((error) => {
+				res.status(500).json({
+					error: 'There was an error while saving the Bear to the Database'
+				})
+			});
+		}
 });
+
+server.get('/api/bears', (req, res) => {
+	Bear.find({})
+		.then((bears) => {
+			res.status(200).json(bears);
+		})
+		.catch(() => {
+			res.status(500).json({
+				error: 'The information could not be retrieved.'
+			});
+		});
+});
+
+server.get('/api/bears/:id', (req, res) => {
+	const { id } = req.params;
+
+	Bear.findById(id)
+		.then((bear) => {
+			res.status(200).json(bear);
+		})
+		.catch((error) => {
+			res.status(500).json({
+				error: 'The information could not be retrieved.'
+			});
+		});
+});
+
+const port = process.env.PORT || 5005;
+
+mongoose.Promise = global.Promise;
+mongoose
+	.connect('mongodb://localhost:27017/bears')
+	.then(() => {
+		server.listen(port, () => {
+  			console.log(`Successfully connected to MongoDB`);
+		});
+	})
+	.catch((error) => {
+		console.log(`Database connection failed`);
+	});
+
+
+
